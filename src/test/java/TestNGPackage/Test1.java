968f74f689a64114fead470029b1f1fc043c5f06
Sandeep Aggarwal <sandeep.agarwal@mycom-internal.com>
2017-05-22 21:50:03 +0530

    Initial Commit

---
+++ src/test/java/TestNGPackage/Test1.java
@@ -0,0 +1,12 @@
+package TestNGPackage;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void doLogin(){
+		System.out.println("Performing the Login operation");
+	}
+
+}
