4783101db6edd5c636ef88373520f412105383bf
Sandeep Aggarwal <sandeep.agarwal@mycom-internal.com>
2017-05-22 22:34:11 +0530

    Third Comment

--- src/test/java/TestNGPackage/Test1.java
+++ src/test/java/TestNGPackage/Test1.java
@@ -1,5 +1,8 @@
 package TestNGPackage;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class Test1 {
@@ -8,6 +11,15 @@ public class Test1 {
 	public void doLogin(){
 		System.out.println("Performing the Login operation");
 		System.out.println("Performing the Login operation");
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.get("http://newtours.demoaut.com");
+		driver.findElement(By.name("userName")).sendKeys("tutorial");
+		driver.findElement(By.name("password")).sendKeys("tutorial");
+		driver.findElement(By.name("login")).click();
+		
+		driver.close();
+		
 	}
 
 }
